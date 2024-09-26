3a352aa19c1c321f72baacb1ecee9cda55f0331e
Harsh Kochar <123500988+HarshKochar9008@users.noreply.github.com>
2024-09-26 16:20:17 +0000

    #

--- frontend/src/contracts/deployed-contract.ts
+++ frontend/src/contracts/deployed-contract.ts
@@ -5,5 +5,6 @@ const deployedAddress = process.env
 	.NEXT_PUBLIC_DEPLOYED_CONTRACT_ADDRESS as Address;
 
 // Type inference correctly
+// Ok
 const ABI = forumAbi;
 export { ABI, deployedAddress };
